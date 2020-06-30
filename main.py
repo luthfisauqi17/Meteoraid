0e35b5a47c6265ae27c32c5b2afc39661ea60278
luthfisauqi17 <sauqiluthfi877@gmail.com>
2020-06-30 21:37:23 +0700

    1.4. Add keyboard control for player

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ pygame.display.set_icon(gameIcon)
 playerImg = pygame.image.load("assets/player.png")
 playerX = 370
 playerY = 480
+playerXSpeed = 1
+playerXChange = 0
 
 # Functions
 # Player
@@ -34,10 +36,24 @@ while running:
     # Background
     screen.fill((0, 0, 0))
 
+    # Events handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        # Keyboard control stroke
+        if event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_LEFT:
+            playerXChange = -playerXSpeed
+          if event.key == pygame.K_RIGHT:
+            playerXChange = playerXSpeed
+          
+        if event.type == pygame.KEYUP:
+          if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            playerXChange = 0
+
+    # Player move update
+    playerX += playerXChange
     player(playerX, playerY)
 
     # Update screen
