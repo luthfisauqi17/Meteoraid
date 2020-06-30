9176837a8f6c5f10abd240b5afa2b14a97a3186a
luthfisauqi17 <sauqiluthfi877@gmail.com>
2020-06-30 21:01:29 +0700

    1.3. Insert player model

--- main.py
+++ main.py
@@ -13,9 +13,32 @@ pygame.display.set_caption("Meteoraid")
 gameIcon = pygame.image.load("assets/icon.png")
 pygame.display.set_icon(gameIcon)
 
+# Characters
+# Player
+playerImg = pygame.image.load("assets/player.png")
+playerX = 370
+playerY = 480
+
+# Functions
+# Player
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
 # Loop
 running = True
 while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
+
+    # Background
+    screen.fill((0, 0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    player(playerX, playerY)
+
+    # Update screen
+    pygame.display.update()
