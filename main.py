46e7ba87dbb2efded551eeb6ca97b0467e8bb60c
luthfisauqi17 <sauqiluthfi877@gmail.com>
2020-07-01 10:17:29 +0700

    2.4. Add score text

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 import random
 import math
 
+# Initialization 
+pygame.init()
 
 # Window size
 (width, height) = (800, 600)
@@ -21,6 +23,15 @@ pygame.display.set_icon(gameIcon)
 
 # Score
 scoreValue = 0
+font = pygame.font.Font("freesansbold.ttf", 32)
+textX = 10
+textY = 10
+
+
+def showScore(x, y):
+    score = font.render("Score: " + str(scoreValue), True, (255, 255, 255))
+    screen.blit(score, (x, y))
+
 
 # Characters
 # Player
@@ -29,6 +40,11 @@ playerX = 370
 playerY = 480
 playerXSpeed = 3
 playerXChange = 0
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
 
 # Target
 targetImg = []
@@ -43,6 +59,11 @@ for i in range(numOfTarget):
     targetY.append(random.randint(15, 50))
     targetYChange.append(2)
 
+
+def target(x, y, i):
+    screen.blit(targetImg[i], (x, y))
+
+
 # Bullet
 bulletImg = pygame.image.load("assets/bullet.png")
 bulletX = 0
@@ -51,24 +72,10 @@ bulletYChange = 3
 bulletState = "ready"
 
 
-# Functions
-# Player
-
-
-def player(x, y):
-    screen.blit(playerImg, (x, y))
-
-
-def target(x, y, i):
-    screen.blit(targetImg[i], (x, y))
-
-
 def fireBullet(x, y):
     global bulletState
     bulletState = "fire"
     screen.blit(bulletImg, (x+16, y+10))
-
-# Collsion detection
 
 
 def isCollided(x1, y1, x2, y2):
@@ -145,6 +152,10 @@ while running:
     if bulletState is "fire":
         fireBullet(bulletX, bulletY)
         bulletY -= bulletYChange
+
+
+    # Show score
+    showScore(textX, textY)
 
     # Update screen
     pygame.display.update()
